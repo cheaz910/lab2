6253d534be3e16dd98d7c118e684d21726c6a2f0
cheaz910 <cheaz910@mail.ru>
2019-11-23 17:26:12 +0500

    rabotaet

--- lab2.ino
+++ lab2.ino
@@ -4,7 +4,7 @@ const int sensorPins[] = {A0, A1, A2};
 const float voltsPerMeasurement = 5.0/1024.0;
 const int minDist = 10;
 
-const byte ledPins[] = {2, 3, 4};
+const byte ledPins[] = {22, 24, 26};
 
 const int playTimeMs = 20000;
 
@@ -14,45 +14,118 @@ const int clkPin = 10;
 const int csPin = 11;
 LedControl ledDisplay = LedControl(dataPin, clkPin, csPin, displaysCount);
 
+const float timerPixelFadeOutTimeMs = playTimeMs / 64; // 312.5
+int timerPixelsFadedOut = 0;
+int timerCurrentRow = 7;
+int timerCurrentCol = 7;
+
+int score = 0;
+int prevPin = -1;
+int nextPin = -1;
+int possibleNextPin = -1;
+
+enum state { 
+  WAIT_ACTION_FROM_USER,
+  WAIT_REMOVE_HAND_FROM_USER,
+  GENERATE_NEXT_PIN,
+  END_GAME
+};
+state currentState = GENERATE_NEXT_PIN;
+
 void setup()
 {
   Serial.begin(115200);
 
   ledDisplay.shutdown(0, false);
-  ledDisplay.setIntensity(0, 16);
+  ledDisplay.setIntensity(0, 4);
   ledDisplay.clearDisplay(0);
+  
+  fillDisplay(true);
+  currentState = GENERATE_NEXT_PIN;
 }
 
 void loop() {
-  int score = 0;
-  while (millis() < playTimeMs) {
-    int nextSensorPin = random(3);
-    score += playPart(nextSensorPin);
+  switch(currentState) {
+    case GENERATE_NEXT_PIN:
+      generate_next_pin();
+      break;
+    case WAIT_REMOVE_HAND_FROM_USER:
+      wait_remove_hand_from_user();
+      break;
+    case WAIT_ACTION_FROM_USER:
+      wait_action_from_user();
+      break;
+    case END_GAME:
+      showScoreOnDisplay(score);
+      delay(60000);
+      break;
   }
-  Serial.println(score);
-  showScoreOnDisplay(score);
-  delay(10000);
-  ledDisplay.clearDisplay(0);
+  checkTime();
+}
+
+void generate_next_pin() {
+  possibleNextPin = random(3);
+  if (possibleNextPin == prevPin) {
+    return;
+  }
+  nextPin = possibleNextPin;
+  currentState = WAIT_ACTION_FROM_USER;
+}
+
+void wait_remove_hand_from_user() {
+  if (readDist(0) <= minDist || readDist(1) <= minDist || readDist(2) <= minDist) {
+    return;
+  }
+  currentState = GENERATE_NEXT_PIN;
+}
+
+void wait_action_from_user() {
+  switchLed(nextPin, 255);
+  if (readDist(nextPin) > minDist) {
+    return;
+  }
+  switchLed(nextPin, 0);
+  score += 1;
+  currentState = WAIT_REMOVE_HAND_FROM_USER;
+  prevPin = nextPin;
 }
 
 void showScoreOnDisplay(int score) {
   for (int row = 0; row < 8; row++) {
     for (int column = 0; column < 8; column++) {
+      if (score <= 0)
+        break;
+      delay(70);
+      score -= 1;
       ledDisplay.setLed(0, row, column, true);
     }
+    if (score <= 0)
+      break;
   }
 }
 
-int playPart(int pin) {
-  switchLed(pin, 255);
-  while (readDist(pin) > minDist && millis() < playTimeMs) {
-    delay(20);
+void checkTime() {
+  if (floor(millis() / timerPixelFadeOutTimeMs) > timerPixelsFadedOut) {
+    ledDisplay.setLed(0, timerCurrentRow, timerCurrentCol, false);
+    timerPixelsFadedOut += 1;
+    timerCurrentCol -= 1;
+    if (timerCurrentCol == -1) {
+      timerCurrentCol = 7;
+      timerCurrentRow -= 1;
+    }
   }
-  switchLed(pin, 0);
-  if (millis() > playTimeMs) {
-    return 0;
+  if (millis() >= playTimeMs) {
+    currentState = END_GAME;
+    switchLed(nextPin, 0);
   }
-  return 1;
+}
+
+void fillDisplay(bool isOn) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      ledDisplay.setLed(0, i, j, isOn);
+    }
+  }
 }
 
 void switchLed(int pin, int value) {
@@ -61,7 +134,6 @@ void switchLed(int pin, int value) {
 
 float readDist(int sensorPin){
   float volts = readAnalog(sensorPin) * voltsPerMeasurement;
-  // return 65 * pow(volts, -1.10); // for big IR sensor (SHARP 2Y0A02)
   return pow(14.7737/volts, 1.2134); // for small IR sensor (SHARP 2Y0A21)
 }
 
